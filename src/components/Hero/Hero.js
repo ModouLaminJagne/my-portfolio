99e06a0c68118e5c697e5b4cd79f4d4b6b6840ea
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2023-01-01 05:02:39 +0000

    Make the hero component.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Portfolio Website
+      </SectionTitle>
+      <SectionText>
+        The purpose of JavaScript Mastery is to help aspiring and established
+        developers to take their development skills to the next level and build
+        awesome apps.
+      </SectionText>
+      <Button
+        onClick={() => (window.location = 'https://github.com/ModouLaminJagne')}
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
