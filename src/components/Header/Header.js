b8d23b9e7308fe9ab87a1a178f41b807839b1510
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2023-01-01 04:52:13 +0000

    write the heder comppnent.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,53 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+} from './HeaderStyles';
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href="/" legacyBehavior>
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom:'20' }}>
+          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/ModouLaminJagne">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/modou-lamin-jagne-391156222/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.instagram.com/jagneml23/">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
